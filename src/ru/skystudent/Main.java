d0b95e8af087de4ef885d8ad122448ce426b8cdd
algmironov <al.g.mironov@yandex.ru>
2021-12-11 13:40:22 +0300

    Готовое дз по теме 1.7

--- src/ru/skystudent/Main.java
+++ src/ru/skystudent/Main.java
@@ -3,6 +3,28 @@ package ru.skystudent;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Задание 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        // Задание 2
+
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName1);
+
+
+        // Задание 3
+
+        String fullName2 = fullName.replace(" ", "; ");
+        System.out.println("Данные ФИО сотрудника для административного отдела — " + fullName2);
+
+        // Задание 4
+
+        String fullName4 = "Иванов Семён Семёнович";
+        String fullName5 = fullName4.replace('ё', 'е');
+        System.out.println(fullName5);
     }
-}
+        }
